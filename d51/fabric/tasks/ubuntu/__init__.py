2facb54e4b8ee478250db82a742aafa445c4dad9
Travis Swicegood <development@domain51.com>
2010-01-31 15:22:05 -0600

    refactor the code and add docstrings

--- d51/fabric/tasks/ubuntu/__init__.py
+++ d51/fabric/tasks/ubuntu/__init__.py
@@ -8,6 +8,7 @@ import os
 import sys
 
 def add_user(user_to_add=None, path_to_key=None):
+    """Completely initial a new user"""
     if not user_to_add:
         user_to_add = os.environ['USER']
     if not path_to_key:
@@ -17,7 +18,16 @@ def add_user(user_to_add=None, path_to_key=None):
         print "ERROR!  Unable to find your key at: %s" % path_to_key
         sys.exit(1)
 
+    replacements = {
+        'user': user_to_add,
+    }
+    useradd(user_to_add)
+    add_ssh_key_for(user_to_add, path_to_key)
+    reown_home_as(user_to_add)
+    add_sudoer_user(user_to_add)
 
+def useradd(user_to_add):
+    """Call useradd on the remove system"""
     replacements = {
         'user': user_to_add,
     }
@@ -25,7 +35,15 @@ def add_user(user_to_add=None, path_to_key=None):
             --home-dir /home/%(user)s \
             --create-home \
             --user-group \
+            --shell /bin/bash \
+            --password "SrmZ9buSJKlxk" \
             %(user)s""" % replacements)
+
+def add_ssh_key_for(user, path_to_key):
+    """Add the SSH public key for a provided user"""
+    replacements = {
+        'user': user,
+    }
     sudo("mkdir /home/%(user)s/.ssh" % replacements)
     put(path_to_key, "~/%(user)s.pub" % replacements)
     sudo("cat ~/%(user)s.pub | sudo tee /home/%(user)s/.ssh/authorized_keys2" % replacements)
@@ -34,9 +52,18 @@ def add_user(user_to_add=None, path_to_key=None):
     sudo("chmod 0600 /home/%(user)s/.ssh/id_rsa" % replacements)
     sudo("chmod 0600 /home/%(user)s/.ssh/authorized_keys2" % replacements)
     sudo("chmod 0644 /home/%(user)s/.ssh/id_rsa.pub" % replacements)
-    sudo("chown -R %(user)s.%(user)s /home/%(user)s" % replacements)
+
+def reown_home_as(user):
+    """Set a directory as owned by a provided user"""
+    sudo("chown -R %(user)s.%(user)s /home/%(user)s" % {'user': user})
+
+def add_sudoer_user(user):
+    """Make sure the user is part of the admin group"""
+    sudo("usermod --append --groups admin %(user)s" % {'user': user})
 
 def remove_user(user_to_remove=None, remove_files=False):
+    """Remove the user, optionally removing all files"""
     sudo("/usr/sbin/userdel %s" % user_to_remove)
     if remove_files:
         sudo("rm -rf /home/%s" % user_to_remove)
+
